9de561f1f16961f5cd74c29273507589e25c71df
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-12 09:24:10 +0530

    Created a file for 'Blog'

---
+++ src/ui/BlogPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BlogPage = () => {
+    return (
+        <>
+            <header className='bg-[#101923] text-cyan-500 text-2xl font-bold font-mono text-center p-2'>Blog</header>
+            <main className="min-h-screen flex items-center justify-center px-4 bg-linear-to-br from-indigo-950 via-slate-900 to-black text-white overflow-hidden relative">
+
+            </main>
+        </>
+    )
+}
+
+export default BlogPage;
