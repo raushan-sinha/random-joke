94292b716930f6aff5dc0a1f48136f5c45b3633b
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-13 12:41:59 +0530

    Created a file for storing data of 'Blog'

---
+++ src/data/blogData.js
@@ -0,0 +1,5 @@
+export const BLOGDATA = {
+    heading: "What is Context API?",
+    info:
+        "Context API lets you share data across components without passing props manually at every level."
+}
