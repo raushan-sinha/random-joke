cc6d973580e738c73ed531516ade9e8d2b460f48
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-13 21:04:43 +0530

    Added new content

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,5 +1,5 @@
 export const BLOGDATA = {
-    heading: "What is Context API?",
+    heading: "Overview",
     info:
-        "Context API lets you share data across components without passing props manually at every level."
+        "This project is a lightweight, high-impact web app that delivers random jokes in real time using a free public Joke API [https://official-joke-api.appspot.com/random_joke]. The goal was to create a fun, responsive user experience while demonstrating strong frontend fundamentals like API consumption, async handling, and clean UI rendering.",
 }
